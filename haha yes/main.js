a3095d2e3dc9831186ea6cb65a2b63c7cb5abfc3
randomhuman1234 <85280593+randomhuman1234@users.noreply.github.com>
2021-07-14 19:05:09 +0530

    Add files via upload

---
+++ haha yes/main.js
@@ -0,0 +1,11 @@
+ function skylines() {
+     document.getElementById("yes").src = "https://th.bing.com/th/id/R.f2a545030cd5e42edee602f556cd1991?rik=FENC1Ea%2bVZTHrQ&riu=http%3a%2f%2fwww.macgamestore.com%2fimages_screenshots%2fcities-skylines-natural-disasters-46900.jpeg&ehk=MVRisq1AOGS8%2fpAxk1PnqTmxUezrpVWHJzuzFGbFGv0%3d&risl=&pid=ImgRaw";
+     document.getElementById("afterclickyes").style.visibility = "hidden";
+     document.getElementById("beforeclick").style.visibility = "visible";
+ }
+
+ function skylinesyes() {
+     document.getElementById("yes").src = "https://www.pcgamesn.com/wp-content/uploads/2021/01/city-builders-stories-simcity.jpg";
+     document.getElementById("beforeclick").style.visibility = "hidden";
+     document.getElementById("afterclickyes").style.visibility = "visible";
+ }
